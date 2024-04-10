0533983fdceba9e8cce3b21f0a3d588df1e0fe0e
BigBabyofTel <augustus.tb@outlook.com>
2024-04-10 10:09:56 +0200

    Initial comminit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lustria } from 'next/font/google'
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lustria({weight: '400', subsets: ['latin']});
+export const inter = Inter({subsets: ['latin']});
